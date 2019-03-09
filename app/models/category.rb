79581a88ad425bf54e22c46c3b9d5be03a295c0c
Diego Selzlein <diegoselzlein@gmail.com>
2019-03-09 18:16:12 -0300

    Scopes

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
   has_many :products
+
+  scope :active, -> { where(active: true) }
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,14 @@
 class Product < ApplicationRecord
   belongs_to :category
+
+  scope :active, -> { joins(:category).where(active: true).where('categories.active = ?', true) }
+  scope :search, ->(query) { where('name ilike :query OR sku ilike :query', query: "%#{query}%") }
+
+  def self.above_price(price)
+    where('price >= ?', price)
+  end
+
+  def self.below_price(price)
+    where('price <= ?', price)
+  end
 end
