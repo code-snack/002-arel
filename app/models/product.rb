8578b94e6e6e625ebd1d5a32faff73c24e597e57
Diego Selzlein <diegoselzlein@gmail.com>
2019-03-09 23:19:40 -0300

    After.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,14 +1,18 @@
 class Product < ApplicationRecord
   belongs_to :category
 
-  scope :active, -> { joins(:category).where(active: true).where('categories.active = ?', true) }
-  scope :search, ->(query) { where('name ilike :query OR sku ilike :query', query: "%#{query}%") }
+  scope :active, -> { joins(:category).where(active: true).merge(Category.active) }
+
+  def self.search(query)
+    query = "%#{query}%"
+    where(arel_table[:name].matches(query).or(arel_table[:sku].matches(query)))
+  end
 
   def self.above_price(price)
-    where('price >= ?', price)
+    where(arel_table[:price].gteq(price))
   end
 
   def self.below_price(price)
-    where('price <= ?', price)
+    where(arel_table[:price].lteq(price))
   end
 end
